227e4d620396453a0ca1a54e14a63071aedec390
Andy Jackson <104866553+andyja@users.noreply.github.com>
2023-10-03 17:31:27 +0100

    template data added

---
+++ data/data.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        title: "Lucca",
+        region: "Tuscany",
+        startDate: "01/01/2001",
+        endDate: "02/01/2001",
+        imageUrl: "https://unsplash.com/photos/qtDcsx4t5H4"
+    }
+]
