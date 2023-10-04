42b41a71578532925ac4df3784c11b2154e4bbae
Andy Jackson <104866553+andyja@users.noreply.github.com>
2023-10-04 09:47:27 +0100

    time of day added to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
 import React from "react"
 
 export default function Header() {
+    const date = new Date();
+    const hours = date.getHours();
+    let day = date.getDay();
+    let timeOfDay
+    let dayOfWeek
+    if (hours < 12) {
+        timeOfDay = "morning";
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon";
+    } else {
+        timeOfDay = "evening";
+    }
     return (
         <div>
-            <header>andy's travel journal</header>
+            <header>{`good ${timeOfDay}, welcome to andy's travel journal`}</header>
         </div>
     )
 }
